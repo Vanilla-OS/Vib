69852cef4e69c80a7c14723c9e9390340d1d7855
Sandeep Nuthalapati <deepsudha@gmail.com>
2024-07-05 12:16:09 +1000

    feat: [#59] cmd implement version flag
    Set version field of the cobra command

--- cmd/root.go
+++ cmd/root.go
@@ -5,9 +5,10 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "vib",
-	Short: "Vib is a tool to build container images from recipes using modules",
-	Long:  "Vib is a tool to build container images from YAML recipes using modules to define the steps to build the image.",
+	Use:     "vib",
+	Short:   "Vib is a tool to build container images from recipes using modules",
+	Long:    "Vib is a tool to build container images from YAML recipes using modules to define the steps to build the image.",
+	Version: "0.7.3",
 }
 
 func init() {
