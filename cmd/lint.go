124c68422fbd95320039ce4c3d9bbbbd24e4297a
Ira Limitanei <lambdaclan.automatic170@simplelogin.fr>
2024-07-18 10:28:07 +0900

    feat: rename test command to lint

--- cmd/lint.go
+++ cmd/lint.go
@@ -7,25 +7,25 @@ import (
 	"github.com/vanilla-os/vib/core"
 )
 
-func NewTestCommand() *cobra.Command {
+func NewLintCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "test",
-		Short: "Test the given recipe",
-		Long:  "Test the given Vib recipe to check if it's valid",
-		RunE:  testCommand,
+		Use:   "lint",
+		Short: "Lint a Vib recipe file",
+		Long:  "Lint a Vib recipe file to check if it is valid",
+		RunE:  lintCommand,
 	}
 	cmd.Flags().SetInterspersed(false)
 
 	return cmd
 }
 
-func testCommand(cmd *cobra.Command, args []string) error {
+func lintCommand(cmd *cobra.Command, args []string) error {
 	if len(args) == 0 {
 		return fmt.Errorf("no recipe path specified")
 	}
 
 	recipePath := args[0]
-	_, err := core.TestRecipe(recipePath)
+	err := core.LintRecipe(recipePath)
 	if err != nil {
 		return err
 	}
