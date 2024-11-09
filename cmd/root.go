b7204f883756775d16d0ff17f38852af9bccaae2
axtloss <axtlos@disroot.org>
2024-11-09 13:44:08 +0100

    fix: exit with non zero returncode on error

--- cmd/root.go
+++ cmd/root.go
@@ -20,6 +20,7 @@ var rootCmd = &cobra.Command{
 	Use:     "vib",
 	Short:   "Vib is a tool to build container images from recipes using modules",
 	Long:    "Vib is a tool to build container images from YAML recipes using modules to define the steps to build the image.",
+	SilenceUsage: true,
 	Version: Version,
 }
 
